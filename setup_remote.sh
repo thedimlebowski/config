10a1027ed7d8faf86d9e5e4a753f8217f7d695d4
Dimitry Foures <dimitry.foures@gmail.com>
2019-01-04 15:25:15 +0000

    config

--- setup_remote.sh
+++ setup_remote.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 if [ "$#" -ne 3 ]; then
-    echo "Usage: setup_remote.sh USERNAME HOSTNAME PUBKEY"
+    echo "Usage: setup_remote.sh USERNAME HOSTNAME KEY"
     exit
 fi
 USERNAME=$1
 HOSTNAME=$2
-PUBKEY=$3
+KEY=$3
 echo Setting up ${USERNAME}@${HOSTNAME}...
-ssh-copy-id -f -i ${PUBKEY} ${USERNAME}@${HOSTNAME}
+ssh-copy-id -f -i ${KEY} ${USERNAME}@${HOSTNAME}
+scp ${KEY} ${USERNAME}@${HOSTNAME}:~/.ssh/$(basename ${KEY})
+ssh ${USERNAME}@${HOSTNAME} "chmod 700 ~/.ssh ; chmod 600 ~/.ssh/authorized_keys ; chmod g-w,o-w ~;"
 scp .* ${USERNAME}@${HOSTNAME}:~/
 echo Done!
